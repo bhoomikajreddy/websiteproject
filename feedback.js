c94cebc1692457bd08ebdbc447b8f777f3cc99b2
bhoomikajreddy <113251846+bhoomikajreddy@users.noreply.github.com>
2023-05-31 11:34:14 +0530

    Add files via upload

---
+++ feedback.js
@@ -0,0 +1,9 @@
+function submit(){
+    var m=alert('Your feedback has been successfully submitted..\n Thank you for your valuable feedback😊');
+    document.getElementById("button").value=m;
+}
+// function submit()
+// {
+//     var m = alert('You have submitted the form. We will contact you shortly.');
+//     document.getElementById('button').value = m;
+// }
